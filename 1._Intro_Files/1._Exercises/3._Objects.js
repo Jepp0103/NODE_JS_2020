71fe93de84f037cd16037d6fcc7667a600f865ff
Jeppe Nannestad Dyekjær <jeppendyekjaer@hotmail.com>
2020-02-09 12:15:39 +0100

    Update 3._Objects.js

--- 1._Intro_Files/1._Exercises/3._Objects.js
+++ 1._Intro_Files/1._Exercises/3._Objects.js
@@ -2,7 +2,8 @@
 // Objects
 // --------------------------------------
 // Exercise 1 - Retrieve value from object by key
-
+console.log("------------------------------------------------")
+console.log("Exercise 1:")
 var myObj = {"message": "Hello, earthling! I bring peace."};
 
 // Log the message 
@@ -12,7 +13,8 @@ console.log(myObj.message);
 
 // --------------------------------------
 // Exercise 2 - Defining an object. 
-
+console.log("------------------------------------------------")
+console.log("Exercise 2:")
 // Create an object that has your name and age. 
 
 var person = {name: "Jeppe", age: 24};
@@ -20,7 +22,8 @@ console.log(person);
 
 // --------------------------------------
 // Exercise 3 - Add a property 
-
+console.log("------------------------------------------------")
+console.log("Exercise 3:")
 var stackOverflow = {isAllowed: true};
 console.log(stackOverflow);
 
@@ -28,7 +31,8 @@ console.log(stackOverflow);
 
 // --------------------------------------
 // Exercise 4 - Remove a property 
-
+console.log("------------------------------------------------")
+console.log("Exercise 4:")
 var thisSong = {"description": "The best song in the world."}
 
 // remove the property "description" and add a property called "about" that should say "Just a tribute." 
