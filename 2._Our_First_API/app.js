11b0725d86d9d4caaf1ac99329016d0537179df9
Jepp0103 <jeppendyekjaer@hotmail.com>
2020-02-21 11:06:40 +0100

    ...

--- 2._Our_First_API/app.js
+++ 2._Our_First_API/app.js
@@ -1,6 +1,8 @@
 //the way to import a package from node modules 
 var express = require("express");
 var app = express();
+const request = require("request");
+
 
 //call back function
 //this is overall called a rest API
@@ -44,8 +46,9 @@ var users = [
 //create a get route on /users
 app.get("/users/:id", (req, res) => {
     console.log(req.params);
-    userById = users.findIndex(user => user.id === req.params);
-    res.send({userById});
+    userById = users.find(user => user.id === 1);
+    console.log(userById);
+    res.send(userById);
 });
 
 
@@ -61,8 +64,22 @@ app.get("/test", (req, res) => {
 //query strings
 app.get("/search", (req, res) => {
     console.log(req.query);
-    res.send(req.query);
+    return res.send(req.query);
+});
 
+app.get("/google", (req, res) => {
+  request('http://www.google.com', function (error, response, body) {
+    console.error('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
+    res.send(body); //defining callback function and displaying page
+  });
+});
+
+//html file
+app.get("/documentationone", (req, res) => {
+    console.log(__dirname)
+    return res.sendFile(__dirname + '/public/documentationone.html');
 });
 
 //listens for a port number
